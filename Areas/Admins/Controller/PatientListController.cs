9bac7b40f275526973519a8cfaa4d20d3a91ff8b
mariamistybarsa <mariabarsa580@gmail.com>
2025-08-16 23:16:29 +0600

    Admin panel patientlist Edit,Delete done

--- Areas/Admins/Controller/PatientListController.cs
+++ Areas/Admins/Controller/PatientListController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Psychiatrist_Management_System.Data;
 using Psychiatrist_Management_System.Models;
+using System.Data;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Psychiatrist_Management_System.Areas.Admins.Controllers
@@ -31,8 +32,47 @@ namespace Psychiatrist_Management_System.Areas.Admins.Controllers
             }
         }
 
-
+        public IActionResult Edit(int id)
+        {
+            using (var connection = _context.CreateConnection())
+            {
+                var p = new DynamicParameters();
+                p.Add("@flag", 9); 
+                p.Add("@Id", id);
+                var data = connection.QueryFirstOrDefault<User>(
+                    "Sp_User",
+                    p,
+                    commandType: System.Data.CommandType.StoredProcedure
+                );
+                return View(data);
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                using (var connection = _context.CreateConnection())
+                {
+                    var parameters = new DynamicParameters();
+                    parameters.Add("@flag", 10); // Delete
+                    parameters.Add("@UserId", id);
+                    connection.Execute(
+                       "Sp_User",
+                        parameters,
+                        commandType: CommandType.StoredProcedure
+                    );
+                }
+                return RedirectToAction("PatientInfo");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
 
 
     }
 }
+
+
+
