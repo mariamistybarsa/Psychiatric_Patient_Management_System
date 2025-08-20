d5d12b7bd4bc5fb74b9fcd4db8aa6a5b363b7c03
mariamistybarsa <mariabarsa580@gmail.com>
2025-08-20 19:01:06 +0600

    User profile without done

---
+++ Areas/Psychologist/Controller/PsyProfileController.cs
@@ -0,0 +1,80 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Psychiatrist_Management_System.Data;
+using Psychiatrist_Management_System.Models;
+using System.Data;
+
+namespace Psychiatrist_Management_System.Areas.Psychologist.Controllers
+{
+    [Area("Psychologist")]
+    public class PsyProfileController : Controller
+    {
+        public readonly DapperContext _context;
+
+        public PsyProfileController(DapperContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                using (var connection = _context.CreateConnection())
+                {
+
+                    var parameters = new DynamicParameters();
+                    parameters.Add("@flag", 2);
+                    var data = connection.Query<PsyProfileVM>(
+
+                      "Sp_PsyProfileVM",
+                      parameters,
+                      commandType: CommandType.StoredProcedure
+                  );
+
+
+                    return View(data);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Save(PsyProfileVM p)
+        {
+            try
+            {
+                using (var connection = _context.CreateConnection())
+                {
+                    var parameters = new DynamicParameters();
+
+                    parameters.Add("@flag", 1);
+                    parameters.Add("@ProfileId", p.ProfileId);
+                    parameters.Add("@PsychiatristId", Convert.ToInt32(HttpContext.Session.GetString("UserId")));
+                    parameters.Add("@Bio", p.Bio);
+                    parameters.Add("@Specialization", p.Specialization);
+                    parameters.Add("@Experience", p.Experience);
+                    parameters.Add("@EmergencyContact", p.EmergencyContact);
+                    parameters.Add("@Imageurl", p.Imageurl);
+
+                    connection.Execute("Sp_PsyProfileVM", parameters, commandType: CommandType.StoredProcedure);
+                }
+
+                return Json(new { success = true, redirectUrl = Url.Action("Index") });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+    }
+}
--- Models/PsyProfileVM.cs
+++ Models/PsyProfileVM.cs
@@ -1,10 +1,15 @@
-﻿//namespace Psychiatrist_Management_System.Models
-//{
-//    public class PsyProfileVM
-//    {
+﻿namespace Psychiatrist_Management_System.Models
+{
+    public class PsyProfileVM
+    {
+        public int ProfileId { get; set; } = 0;
+        public int? PsychiatristId { get; set; }
+        public string? Bio { get; set; }
+      public string? Specialization { get; set; }
+        public string? Experience { get; set; }
+        public string? EmergencyContact { get; set; }
+        public string? Imageurl { get; set; }
 
 
-
-
-//    }
-//}
+    }
+}
