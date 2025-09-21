376581e269d37ab32be67dd4b7073fc27e9533a1
mariamistybarsa <mariabarsa580@gmail.com>
2025-09-21 22:25:47 +0600

    total patient pending running approved of psychiatrist is done

--- Areas/Admins/Controller/DashboardController.cs
+++ Areas/Admins/Controller/DashboardController.cs
@@ -1,13 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Psychiatrist_Management_System.Data;
+using Psychiatrist_Management_System.Models;
+using System.Data;
 
 namespace Authorization.Areas.Admins.Controllers
 {
     [Area("Admins")]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly DapperContext _context;
+        public DashboardController(DapperContext context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            using var conn = _context.CreateConnection();
+            var data = await conn.QueryAsync<dynamic>(
+                "Sp_AdminBookingSummary",
+                commandType: CommandType.StoredProcedure
+            );
+
+            // Render normal table
+            return View(data);
+        }
+        
+
+        
+        [HttpGet]
+        public async Task<IActionResult> GetBookingSummary()
+        {
+            using var conn = _context.CreateConnection();
+            var data = await conn.QueryAsync<dynamic>(
+                "Sp_AdminBookingSummary",
+                commandType: CommandType.StoredProcedure
+            );
+
+            // Render normal table
+            return View(data);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBookingMonthly()
+        {
+            using var conn = _context.CreateConnection();
+
+            var parameters = new DynamicParameters();
+            parameters.Add("@flag", 1); // SP এর জন্য flag parameter
+
+            var data = await conn.QueryAsync<GetMonthlySummary>(
+                "Sp_AdminBookingSummary",
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+
+            return View(data); // normal table view
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBookingMonthlys(int PsychiatristId)
+        {
+            using var conn = _context.CreateConnection();
+
+            var parameters = new DynamicParameters();
+            parameters.Add("@flag", 2); // SP এর জন্য flag parameter
+            parameters.Add("@PsychiatristId", PsychiatristId); // SP এর জন্য PsychiatristId parameter
+
+            var data = await conn.QueryAsync<GetMonthlySummary>(
+                "Sp_AdminBookingSummary",
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+
+            return View(data); // normal table view
+        }
+
+
+
     }
 }
---
+++ Models/GetMonthlySummary.cs
@@ -0,0 +1,14 @@
+﻿namespace Psychiatrist_Management_System.Models
+{
+    public class GetMonthlySummary
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int PsychiatristId { get; set; }
+        public string PsychiatristName { get; set; } // Name যোগ করা
+        public int Pending { get; set; }
+        public int Running { get; set; }
+        public int Completed { get; set; }
+        public int TotalUsers { get; set; }
+    }
+}
